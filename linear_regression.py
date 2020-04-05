f4927d77161454d3652f3584f811a5f08f2e2325
roduquen <47316192+roduquen@users.noreply.github.com>
2020-04-05 18:13:58 +0200

    final push

--- linear_regression.py
+++ linear_regression.py
@@ -1,69 +1,47 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
-def hypothesisReturn(theta0, theta1, value):
-    result = theta0 + theta1 * value
-    return result
+def hypothesisFromModel(path, value):
+    data = parseFile(path)
+    print("The result for the value ", value, " is ", data[0] + data[1] * (value - data[2]) / data[3])
 
-def costFunctionDerivatesComputation(mileage, price, data_nbr, theta0, theta1, alpha):
-    derivate0, derivate1 = 0.0, 0.0
-    for i in range(0, data_nbr):
-        tmp = theta0 + theta1 * mileage[0, i] - price[0, i]
-        derivate0 += tmp
-        derivate1 += tmp * mileage[0, i]
-    return theta0 - alpha * (derivate0 / data_nbr), theta1 - alpha * (derivate1 / data_nbr)
+def costFunctionDerivatesComputation(theta, alpha, data):
+    return np.array([theta[0] - alpha * np.sum(theta[0] + theta[1] * data[:, 0] - data[:, 1]) / data.size * 2, theta[1] - alpha * np.sum((theta[0] + theta[1] * data[:, 0] - data[:, 1]) * data[:, 0]) / data.size * 2])
 
-def normalizeValue(value, min, divider):
-    result = float(value - min) / divider
-    return result
-
-def normalizeVector(vector, size, min, divider):
-    vector_new = vector[0, 0:size]
-    for i in range(0, size):
-        vector_new[0, i] = normalizeValue(vector_new[0, i], min, divider)
-    return vector_new
-
-def gradientDescent(mileage, price, data_nbr, theta0, theta1, alpha, min, divider):
-    prev_theta0, prev_theta1 = theta0 + 1, theta1 + 1
-    mileage_normalized = normalizeVector(mileage, data_nbr, min, divider)
-    epsilon = 1e-20
-    while abs(prev_theta0 - theta0) > epsilon or abs(prev_theta1 - theta1) > epsilon:
-        tmp0, tmp1 = costFunctionDerivatesComputation(mileage_normalized, price, data_nbr, theta0, theta1, alpha)
-        prev_theta0, prev_theta1 = theta0, theta1
-        theta0, theta1 = tmp0, tmp1
-    return theta0, theta1
-
-def convertFileToMatrix(file):
-    try:
-        data_nbr = file.shape[0]
-        mileage = np.matrix(file.iloc[0:data_nbr, 0].values).astype(float)
-        price = np.matrix(file.iloc[0:data_nbr, 1].values).astype(float)
-    except:
-        print("Fail to create the matrices")
-        sys.exit(0)
-    return mileage, price, data_nbr
+def gradientDescent(theta, alpha, data, iters):
+    for i in range(iters):
+        theta[:] = costFunctionDerivatesComputation(theta, alpha, data)[:]
+    return theta
 
 def parseFile(path):
     try:
-        file = pd.read_csv(path)
+        return pd.read_csv(path).to_numpy().astype(float)
     except:
         print("The path does not correspond to a csv file.")
         sys.exit(0)
-    return file
 
-def main():
-    file = parseFile("ressources/data.csv")
-    mileage, price, data_nbr = convertFileToMatrix(file)
-    theta0, theta1 = 0.0, 0.0
+def plotData(data, theta, normalizer):
+    x_axis = data[:, 0]
+    y_axis = data[:, 1]
+    plt.scatter(x_axis, y_axis, c="Blue")
+    plt.plot([data.min(axis=0)[0], data.max(axis=0)[0]], [theta[0], theta[0] + theta[1] * (data.max(axis=0)[0] - normalizer[0]) / normalizer[1]], 'r')
+    plt.xlabel("")
+    plt.ylabel("Price (in euros)")
+    plt.title("Linear regression for car price")
+    plt.show()
+
+def linearRegression(path):
+    data = parseFile(path)
+    data_plot = np.array(data[:]);
+    theta = np.array([0.0, 0.0])
     alpha = 0.1
-    min = mileage.min()
-    divider = float(mileage.max() - min)
-    theta0, theta1 = gradientDescent(mileage, price, data_nbr, theta0, theta1, alpha, min, divider)
-    print("The local optimal for theta0 is ", theta0, " and for theta1 is ", theta1)
-    value = 50000
-    value_normalized = normalizeValue(value, min, divider)
-    print("For the value ", value, ", the result is : ", hypothesisReturn(theta0, theta1, value_normalized))
+    normalizer = np.array([data.min(axis=0)[0], data.max(axis=0)[0] - data.min(axis=0)[0]]).astype(float)
+    data[:, 0] = (data[:,0] - normalizer[0]) / normalizer[1]
+    gradientDescent(theta, alpha, data, 10000)
+    plotData(data_plot, theta, normalizer)
+    pd.DataFrame(np.concatenate([theta, normalizer])).to_csv("ressources/theta.csv", index=False, sep=',')
 
-main()
-
+linearRegression("ressources/data.csv")
+hypothesisFromModel("ressources/theta.csv", 50000)
